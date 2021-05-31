efd41a0c83e70f736be5b4efc1fd0b63df8107d9
scar-2018 <scar20181228@gmail.com>
2021-05-31 06:29:10 +0300

    user menu updated

--- resources/js/components/toolbar/ToolbarUser.vue
+++ resources/js/components/toolbar/ToolbarUser.vue
@@ -13,24 +13,6 @@
       </template>
 
       <v-list dense nav>
-        <v-list-item
-          v-for="(item, index) in menu"
-          :key="index"
-          :to="item.link"
-          :exact="item.exact"
-          :disabled="item.disabled"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon small :class="{ 'grey--text': item.disabled }">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider class="my-1"></v-divider>
-
         <v-list-item @click="logout" link>
           <v-list-item-icon>
             <v-icon small>mdi-logout-variant</v-icon>
@@ -50,15 +32,9 @@ import { mapState, mapActions } from 'vuex'
 export default {
   data() {
     return {
-      menu: [
-        { icon: 'mdi-account-box-outline', key: 'menu.profile', text: 'Profile', link: '/profile' }
-      ]
     }
   },
   computed: {
-    ...mapState({
-      // username: (state) => state.auth.user.username
-    })
   },
   methods: {
     ...mapActions({
